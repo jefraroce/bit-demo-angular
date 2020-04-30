afc90dead9387b6ef2f9b38b26c947fe017fdb8a
Jeisson Francois Rosas Cespedes <jefraroce@hotmail.com>
2020-04-29 20:27:43 -0500

    Removido componente contact-list.

--- src/app/contact-list/contact-list.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'contact-list',
-  templateUrl: './contact-list.component.html',
-  styleUrls: ['./contact-list.component.css']
-})
-export class ContactListComponent { }
